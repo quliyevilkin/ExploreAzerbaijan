2428af1ef5614b19b888b366093565ae57a202de
qlyvilkn <quliyevvilkin578@gmail.com>
2024-04-16 15:58:32 +0400

    v.1.0.0.1

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,4 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export const SITE_TITLE = 'ExploreAzerbaijan.';
+export const SITE_DESCRIPTION = 'Discover the wonders of Azerbaijan with our comprehensive travel guide, offering invaluable insights and tips for an unforgettable journey.';
